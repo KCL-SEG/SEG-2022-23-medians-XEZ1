2645598343d0775bc841dc8bdfdbecfacf4347a3
XEZ1 <xxxaclockworkorangegmail.com>
2022-10-02 02:30:50 +0100

    solve median problem

--- median.py
+++ median.py
@@ -2,11 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
+     try:
+         print("Enter a list of numbers separated by commas: ")
+         numbers = [float(value) for value in input().split(",")]
+     except ValueError:
+         print("Some input could not be converted to a number!")
+     else:
+         break
+
+numbers.sort()
+if len(numbers) % 2 == 0:
+      leftMid = numbers[int((len(numbers) / 2) - 1)]
+      rightMid = numbers[int(len(numbers) / 2)]
+      mid = float(leftMid + rightMid) / 2
+else:
+      mid = float(numbers[int(len(numbers) / 2)])
+print(mid)
+
+
